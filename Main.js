00135763451ca77e5ea3654fa6f51b282b0187c1
khanhdzx <khanhvannguyen2303@gmail.com>
2022-08-10 23:17:51 +0700

    lan1

---
+++ Main.js
@@ -0,0 +1,82 @@
+//bài 1
+// let myColor = ["Red", "Green", "White", "Black"];
+// let a = myColor.join(",");
+// console.log(a);
+
+// let myColor = ["Red", "Green", "White", "Black"];
+// let a = myColor.join("+");
+// console.log(a);
+
+// bài 2
+// let number = prompt('nhập dãy số bất kỳ ').split('');
+// let b=[];
+// for(let i = 0; i < number.length; i++) {
+//     b.push(number[i]);
+// }
+// let a = b.join('-');
+// console.log(a);
+
+// bài 3
+// let text = prompt('Nhập chữ');
+// let chuInHoa = "QWERTYUIOPASDFGHJKLZXCVBNM";
+// let chuInThuong = "qwertyuiopasdfghjklzxcvbnm";
+// let result=[];
+// for (let i=0; i<text.length;i++){
+//     if (chuInHoa.indexOf(text[i])===-1){
+//         result.push(text[i].toUpperCase());
+//     }else if (chuInThuong.indexOf(text[i])===-1){
+//         result.push(text[i].toLowerCase());
+//     }else{
+//         result.push(text[i]);
+//     }
+// }
+// console.log(result.join(""));
+// console.log(text);
+
+// bài 4
+// let a = prompt('enter a squence of number, separated bay commas (,)').split(',');
+// let sum = 0;
+// for(let i = 0; i < a.length; i++) {
+//     sum += Number(a[i]);
+// }
+// console.log(sum);
+
+// bài 8
+// let a = prompt('enter a sequence of names');
+// let b = a.split(',');
+// let c =[];
+// for (let i = 0; i < b.length; i++) {
+//     c.push("<"+b[i]+">");
+// }
+// console.log(b +"=>"+c);
+
+// bài 9
+// let a = prompt('nhập dãy số');
+// let b = a.split(',');
+// let c = [];
+// for (let i = 0; i < b.length; i++) {
+//     if (Number(b[i]) % 2 == 1) {
+//         c.push(b[i]);
+//     }
+// }
+// console.log(b +"=>"+ c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
